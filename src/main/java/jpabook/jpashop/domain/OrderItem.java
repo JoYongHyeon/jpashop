cbcb155fbde522d2dbda89975a82f519111daa9b
yonghyeonjooo <yh.jo@okestro.com>
2023-12-12 11:46:01 +0900

    1212commit

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,7 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.*;
@@ -10,6 +12,7 @@ import static jakarta.persistence.FetchType.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -29,4 +32,33 @@ public class OrderItem {
 
     private int count; // 주문 수량
 
+//    protected OrderItem() {       //  @NoArgsConstructor(access = AccessLevel.PROTECTED) 같은 기능
+        // OrderService 의 41 번째 줄
+        // protected 생성자가 있다면 JAP 에서는 쓰지 말라는 말.
+//    }
+
+    //== 생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    // == 비즈니스 로직 == //
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /*
+        주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
